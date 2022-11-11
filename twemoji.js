198c9e380c653c7854280191f7cba4ef0f9f2110
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2022-11-10 22:33:27 -0500

    Update twemoji.js

--- twemoji.js
+++ twemoji.js
@@ -1,1 +1,16 @@
-//here twemoji startup soon
+//twemoji startup
+let div = document.createElement('div');
+div.textContent = 'I \u2764\uFE0F emoji!';
+document.body.appendChild(div);
+
+twemoji.parse(document.body);
+
+let img = div.querySelector('img');
+
+// note the div is preserved
+img.parentNode === div; // true
+
+img.src;        // https://twemoji.maxcdn.com/v/latest/72x72/2764.png
+img.alt;        // \u2764\uFE0F
+img.className;  // emoji
+img.draggable;  // false
