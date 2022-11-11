197b156b49ccbf179370c9a779aa13c165d0bba0
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2022-11-11 00:23:01 -0500

    Update broken

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ body.onload = () {
 		document.getElementById("user").innerHTML = ProfileName;
 		document.getElementById("handle").innerHTML = "@" + Handle;
 	}
+}
 //twitter counts startup
 	let likemoji = '<svg viewBox="0 0 24 24" aria-hidden="true" class="likesvg"><g><path d="M20.884 13.19c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path></g></svg>';
 	let retweetmoji = '<svg viewBox="0 0 24 24" aria-hidden="true" class="retweetssvg"><g><path d="M4.5 3.88l4.432 4.14-1.364 1.46L5.5 7.55V16c0 1.1.896 2 2 2H13v2H7.5c-2.209 0-4-1.79-4-4V7.55L1.432 9.48.068 8.02 4.5 3.88zM16.5 6H11V4h5.5c2.209 0 4 1.79 4 4v8.45l2.068-1.93 1.364 1.46-4.432 4.14-4.432-4.14 1.364-1.46 2.068 1.93V8c0-1.1-.896-2-2-2z"></path></g></svg> ';
