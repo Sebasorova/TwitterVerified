68b4900f3a00b321f383437e3479747ea3b2631f
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2022-11-10 23:53:42 -0500

    Updated to parse to svg, not png

--- twemoji.js
+++ twemoji.js
@@ -1,4 +1,8 @@
 //twemoji startup, may not work rn
 window.onload = function() {    
-  twemoji.parse(document.body);
+            // Parses the document body and    
+            // inserts <img> tags in place of Unicode Emojis    
+            twemoji.parse(document.body, 
+                    {folder: 'svg', ext: '.svg'} // This is to specify to Twemoji to use SVGs and not PNGs
+            );
 }
