12632f9e607621f1b5601a1b2a89a6c5bfb61973
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2022-11-11 00:00:55 -0500

    Changed to setinterval to update every 1ms

--- twemoji.js
+++ twemoji.js
@@ -1,8 +1,6 @@
-//twemoji startup, may not work rn
-window.onload = function() {    
-            // Parses the document body and    
-            // inserts <img> tags in place of Unicode Emojis    
+window.setInterval(
+	function() {     
             twemoji.parse(document.body, 
-                    {folder: 'svg', ext: '.svg'} // This is to specify to Twemoji to use SVGs and not PNGs
+                    {folder: 'svg', ext: '.svg'}
             );
-}
+}, 1);
